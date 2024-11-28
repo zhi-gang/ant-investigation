75b2f82a925e2a28622abe5b1b660716d3dcea98
Zhigang <zhi-gang.yan@outlook.com>
2024-11-28 15:53:52 +0800

    v2

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {FormArray, FormBuilder, FormGroup, ReactiveFormsModule, Validators} from '@angular/forms';
 import {NzFormItemComponent} from 'ng-zorro-antd/form';
 import {NzColDirective} from 'ng-zorro-antd/grid';
 import {NzTableComponent} from 'ng-zorro-antd/table';
-import {JsonPipe, NgForOf} from '@angular/common';
+import {JsonPipe, NgForOf, NgIf} from '@angular/common';
 import {NzSwitchComponent} from 'ng-zorro-antd/switch';
 import {NzInputDirective} from 'ng-zorro-antd/input';
 
@@ -18,60 +18,142 @@ import {NzInputDirective} from 'ng-zorro-antd/input';
     JsonPipe,
     NzSwitchComponent,
     NgForOf,
-    NzInputDirective
+    NzInputDirective,
+    NgIf
   ],
   styleUrls: ['./app.component.css']
 })
 export class AppComponent implements OnInit {
   formGroup: FormGroup;
+  spec = [
+    {
+      "specId": "s20241127162728654NAR",
+      "specName": "宽",
+      "displayOrder": 1,
+      "values": [
+        {
+          "valueId": "sv20241127162728654abW",
+          "value": "500",
+          "displayOrder": 0
+        },
+        {
+          "valueId": "sv20241127162728654fS6",
+          "value": "600",
+          "displayOrder": 1
+        }
+      ]
+    },
+    {
+      "specId": "s20241127162728631fw4",
+      "specName": "长",
+      "displayOrder": 0,
+      "values": [
+        {
+          "valueId": "sv20241127162728632YtI",
+          "value": "200",
+          "displayOrder": 0
+        },
+        {
+          "valueId": "sv20241127162728632H3G",
+          "value": "300",
+          "displayOrder": 1
+        }
+      ]
+    }
+  ];
   skus = {
-    specs: [
-      { specId: 's20241127162728631fw4', specName: '长' },
-      { specId: 's20241127162728654NAR', specName: '宽' }
+    "specs": [
+      {
+        "specId": "s20241127162728631fw4",
+        "specName": "长"
+      },
+      {
+        "specId": "s20241127162728654NAR",
+        "specName": "宽"
+      }
     ],
-    skuSnapshotList: [
+    "skuSnapshotList": [
       {
-        skuId: 'sku202411271627287013kb',
-        displayName: '规格 1',
-        values: ['200', '500'],
-        stock: 0,
-        price: 4.3,
-        weight: 0,
-        sellable: true
+        "_type_": "SkuSnapshot",
+        "skuId": "sku202411271627287013kb",
+        "displayName": "规格 1",
+        "values": [
+          "200",
+          "500"
+        ],
+        "valueIds": [
+          "sv20241127162728632YtI",
+          "sv20241127162728654abW"
+        ],
+        "stock": 0,
+        "price": 4.3,
+        "weight": 0,
+        "state": "published",
+        "displayOrder": 0,
+        "sellable": true
       },
       {
-        skuId: 'sku20241127162728733nEM',
-        displayName: '规格 2',
-        values: ['200', '600'],
-        stock: 0,
-        price: 4.23,
-        weight: 0,
-        sellable: true
+        "_type_": "SkuSnapshot",
+        "skuId": "sku20241127162728733nEM",
+        "displayName": "规格 2",
+        "values": [
+          "200",
+          "600"
+        ],
+        "valueIds": [
+          "sv20241127162728632YtI",
+          "sv20241127162728654fS6"
+        ],
+        "stock": 0,
+        "price": 4.23,
+        "weight": 0,
+        "state": "published",
+        "displayOrder": 1,
+        "sellable": true
       },
       {
-        skuId: 'sku202411271627287565mY',
-        displayName: '规格 3',
-        values: ['300', '500'],
-        stock: 0,
-        price: 2.3,
-        weight: 0,
-        sellable: false
+        "_type_": "SkuSnapshot",
+        "skuId": "sku202411271627287565mY",
+        "displayName": "规格 3",
+        "values": [
+          "300",
+          "500"
+        ],
+        "valueIds": [
+          "sv20241127162728632H3G",
+          "sv20241127162728654abW"
+        ],
+        "stock": 0,
+        "price": 2.3,
+        "weight": 0,
+        "state": "published",
+        "displayOrder": 2,
+        "sellable": false
       },
       {
-        skuId: 'sku20241127162728770AVk',
-        displayName: '规格 4',
-        values: ['300', '600'],
-        stock: 0,
-        price: 4.3,
-        weight: 0,
-        sellable: true
+        "_type_": "SkuSnapshot",
+        "skuId": "sku20241127162728770AVk",
+        "displayName": "规格 4",
+        "values": [
+          "300",
+          "600"
+        ],
+        "valueIds": [
+          "sv20241127162728632H3G",
+          "sv20241127162728654fS6"
+        ],
+        "stock": 0,
+        "price": 4.3,
+        "weight": 0,
+        "state": "published",
+        "displayOrder": 3,
+        "sellable": true
       }
     ]
   };
   specColumns: any;
   skuTableData: any;
   editId: string | null = null;
-
 
 
   constructor(private fb: FormBuilder) {
@@ -95,6 +177,7 @@ export class AppComponent implements OnInit {
   get skuControls(): FormArray {
     return this.formGroup.get('skuControls') as FormArray;
   }
+
   getFormGroup(index: number): FormGroup {
     return this.skuControls.at(index) as FormGroup;
   }
